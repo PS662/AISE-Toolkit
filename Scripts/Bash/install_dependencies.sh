dda3c79d3cca22b7bbbfb95f85c034a58b13c5f2
Prateek Singh <singh.prateek662@gmail.com>
2024-07-08 00:28:27 +1000

    Base installer script

---
+++ Scripts/Bash/install_dependencies.sh
@@ -0,0 +1,5 @@
+sudo apt update
+
+sudo apt install -y build-essential cmake gdb valgrind git vim \
+                    libboost-all-dev cmake-qt-gui pkg-config doxygen \
+                    clang clang-tools lcov
