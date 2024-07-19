2289bae95efb2b597218a2c91b4a41ce43b4a8c0
Prateek Singh <singh.prateek662@gmail.com>
2024-07-19 19:43:15 +1000

    minor changes

--- Scripts/Python/download_data.py
+++ Scripts/Python/download_data.py
@@ -2,15 +2,29 @@ import os
 import sys
 import subprocess
 import argparse
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-def download_from_git(destination_dir, git_url):
+def download_from_git(git_url, destination_dir):
     """Download a repository from a given Git URL to the specified directory."""
+    repo_name = git_url.split('/')[-1]
+    if repo_name.endswith('.git'):
+        repo_name = repo_name[:-4]  # Remove the .git from the end
+    
+    # Create the full path where the repo will be cloned
+    full_path = os.path.join(destination_dir, repo_name)
+
+    # Create the destination directory if it doesn't exist
+    if not os.path.exists(full_path):
+        os.makedirs(full_path)
     try:
-        subprocess.run(['git', 'clone', git_url, destination_dir], check=True)
+        subprocess.run(['git', 'clone', git_url, full_path], check=True)
         print(f"Repository successfully cloned to {destination_dir}")
     except subprocess.CalledProcessError as e:
         print(f"An error occurred while cloning the repository: {e}")
         sys.exit(1)
+
 
 def main():
     parser = argparse.ArgumentParser(description="Download datasets to a specified directory.")
@@ -18,14 +32,11 @@ def main():
 
     subparsers = parser.add_subparsers(dest='command', required=True, help='Sub-command help')
 
-    # Subparser for downloading from Git
     parser_git = subparsers.add_parser('download-from-git', help='Download a repository from a Git URL')
     parser_git.add_argument('--git-url', type=str, required=True, help='URL of the Git repository')
-    parser_git.set_defaults(func=download_from_git)
 
-    # Subparser for downloading all datasets
-    parser_all = subparsers.add_parser('download-all', help='Download all datasets')
-    parser_all.set_defaults(func=lambda args: download_from_git(args.dest, 'https://github.com/intel-iot-devkit/sample-videos'))
+    parser_url = subparsers.add_parser('download-from-url', help='Download content from a URL')
+    parser_url.add_argument('--url', type=str, required=True, help='URL to download content from')
 
     args = parser.parse_args()
 
@@ -42,9 +53,11 @@ def main():
 
     # Proceed to download the specified datasets
     if args.command == 'download-from-git':
-        args.func(destination_dir, args.git_url)
-    elif args.command == 'download-all':
-        args.func(destination_dir, 'https://github.com/intel-iot-devkit/sample-videos')
+        download_from_git(args.git_url, destination_dir)
+    else:
+        parser.print_help()
+        print("\nError: You must specify a subcommand (download-from-git or download-from-url).")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
